8888569b10fab40b53419452fdffbf157f0b624e
Parnab03 <pb7439578071@gmail.com>
2024-07-09 12:56:06 +0530

    Practise set two added

---
+++ set 2/script .js
@@ -0,0 +1,5 @@
+const myName = 'Parnab';
+document.querySelector('#name_display').innerText = myName;
+let cartValue = `Your final myntra bag ammount is ₹${Math.round(123.34)}`;
+console.log(cartValue);
+console.log(eval('2+3+9-4*8'));
